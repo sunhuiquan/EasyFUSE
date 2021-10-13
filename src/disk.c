0615a899d41fac17ac4ad96f6acf9a2afa4d2693
sunhuiquan <sunhuiquan@yeah.net>
2021-10-13 14:01:42 +0800

    add

--- src/disk.c
+++ src/disk.c
@@ -9,6 +9,7 @@
 static int disk_fd;
 static struct super_block superblock;
 
+/* 初始化磁盘 */
 void init_disk(const char *path)
 {
 	struct stat sbuf;
@@ -24,10 +25,12 @@ void init_disk(const char *path)
 	if (real_size <= 64 * BLOCK_SIZE)
 		err_exit("diskimg too small");
 
-	// 初始化 super block 块
 	// 现在未加入日志支持，无logging layer的情况
 	// 0          | 1           | 2 ...        | rest
 	// boot block | super block | inode blocks | data blocks
+
+	// to do
+	// 初始化 super block 块
 	superblock.block_num = real_size / BLOCK_SIZE; // 向下舍入
 	superblock.inode_block_startno = 2;
 	// superblock.data_block_startno = ?;
@@ -38,11 +41,16 @@ void init_disk(const char *path)
 	for (int i = 0; i < superblock.block_num; ++i)
 	{
 		if (read(disk_fd, temp, BLOCK_SIZE) != BLOCK_SIZE)
-			err_exit("read");
+			err_exit("clear disk");
 		memset(temp, 0, BLOCK_SIZE);
 		if (write(disk_fd, temp, BLOCK_SIZE) != BLOCK_SIZE)
-			err_exit("write");
+			err_exit("clear disk");
 	}
+}
 
-	// to do
+/* 从磁盘中读取物理块到内存中的block cache数组缓冲结构，注意这里的 
+ * blockno 是物理块号，不过我们这个 FS 中的物理块号和逻辑块号是一
+ * 对一的，不然需要分析逻辑块地址映射到具体的物理块 */
+struct cache_block *block_get(int blockno){
+	
 }
--- src/disk.h
+++ src/disk.h
@@ -1,17 +1,12 @@
 #ifndef DISK_H
 #define DISK_H
 
+#include "fs.h"
+
 /* 初始化磁盘 */
 void init_disk(const char *path);
 
-/* 读取物理块进入内存，放入空闲的 block cache 中 */
-// int block_get(uint no) // 逻辑块号
-// {
-// 	struct cache_block buf;
-// 	off_t offset = no * BLOCK_SIZE;
-// 	if (read(disk_fd, buf.data, BLOCK_SIZE) != BLOCK_SIZE)
-// 		err_exit("read");
-// 	// 这里的read是读肯定有内容的文件，不错误的情况下一定是返回要求我们读的大小
-// }
+/* 从磁盘中读取物理块到内存中的block cache数组缓冲结构 */
+struct cache_block *block_get(int blockno);
 
 #endif
