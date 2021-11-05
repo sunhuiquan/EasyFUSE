e6ca5857bb3f930afa0f61b80864f41b16cd4d7b
sunhuiquan <sunhuiquan@yeah.net>
2021-11-05 10:30:12 +0800

    add

--- src/log.c
+++ src/log.c
@@ -3,8 +3,13 @@
 #include "disk.h"
 #include "fs.h"
 #include "log.h"
+#include "block_cache.h"
 
+#include <string.h>
 #include <pthread.h>
+
+static int recover_from_log_disk();
+static int disk_read_log_head();
 
 struct log_head
 {
@@ -12,17 +17,15 @@ struct log_head
 	int logs[LOG_BLOCK_NUM]; // 每个日志块要写入的数据块逻辑块号
 };
 
-struct log_cache
+struct log
 {
 	// struct spinlock lock; 自旋锁？？??
-	int startno;			  // 日志块第一个的块号（即头结点块）
-	int num;				  // 日志块数量
 	int syscall_num;		  // 当前正在写入日志块的写系统调用数量
 	int is_committing;		  // 是否正在提交（commit）
 	pthread_mutex_t log_lock; // 互斥锁
 	struct log_head head;	  // 日志层的头节点块
 };
-struct log_cache log;
+struct log log;
 
 /* 初始化日志层，这是内核加载文件系统，文件系统初始化的一部分，是开机启动操作系统的一部分，
  * 你要考虑上次关机是否正常，比如因断电关机，所以检测磁盘上的logging layer进行日志恢复操作。
@@ -31,9 +34,26 @@ int init_log()
 {
 	if (pthread_mutex_init(&log.log_lock, NULL) == -1)
 		return -1;
-	log.startno = superblock.log_block_startno;
-	log.num = superblock.log_block_num;
-	log.syscall_num = 0;
-	log.is_committing = 0;
-	// recovering from disk
+	if (recover_from_log_disk() == -1)
+		return -1;
+}
+
+static int recover_from_log_disk()
+{
+	if (disk_read_log_head() == -1)
+		return -1;
+	// to do
+	return 0;
+}
+
+/* 从磁盘读取头日志块到内存 */
+static int disk_read_log_head()
+{
+	struct cache_block *bbuf = block_read(superblock.log_block_startno);
+	if (bbuf == NULL)
+		return -1;
+	memmove(&log.head, bbuf->data, sizeof(struct log_head));
+	if (block_unlock_then_reduce_ref(bbuf) == -1)
+		return -1;
+	return 0;
 }
