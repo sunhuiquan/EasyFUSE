b2f1cc0eedcb19b57ca1892fc1b3e7accc954116
sunhuiquan <sunhuiquan@yeah.net>
2021-11-10 08:21:33 +0800

    add

--- src/fuse_fs_init.c
+++ src/fuse_fs_init.c
@@ -1,9 +1,12 @@
 #include "fuse_fs_calls.h"
 #include "fs.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /* 开机系统启动后加载文件系统 */
 void *fuse_fs_init(void)
 {
+	printf("文件系统开始加载\n");
 	if (load_disk(DISK_FILE_PATH) == -1) // 加载磁盘，读取superblock到内存的全局变量中
 		err_exit("load_disk(argv[1])");
 	if (inode_cache_init() == -1) // 初始化 inode cache
@@ -12,4 +15,6 @@ void *fuse_fs_init(void)
 		err_exit("init_block_cache_block");
 	if (init_log() == -1) // 初始化log，并进行磁盘恢复操作
 		err_exit("init_log");
+	printf("文件系统加载结束\n");
+	return NULL; // 我们用不到那个private_data字段，返回NULL即可
 }
